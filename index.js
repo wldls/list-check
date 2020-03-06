13fca91d92cb5269acefe5ef86bc967a0b4b6cc1
jiin <dslet2343@gmail.com>
2020-03-06 18:41:12 +0900

    webpack 설정

---
+++ index.js
@@ -0,0 +1,15 @@
+import './src/css/common.css';
+// import _ from 'lodash';
+
+// console.log('css loaded');
+
+// function component() {
+// 	var element = document.createElement('div');
+  
+// 	/* lodash is required for the next line to work */
+// 	element.innerHTML = _.join(['Hello','webpack'], ' ');
+  
+// 	return element;
+// }
+
+// document.body.appendChild(component());
